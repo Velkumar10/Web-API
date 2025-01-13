4a806d9c42bfe75fea2567a77f5eaa1d63b2a8eb
Velkumar Rajagopal <r.velkumar10@gmail.com>
2025-01-13 17:18:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,19 @@ app = Flask(__name__)
 def ping():
     return 'Pong'
 
-@app.route('/startApi', methods=['GET'])
+@app.route('/XMII/Runner', methods=['GET'])
 def startApi():
-    sfcNo = request.args.get('sfc')  # Get the 'sfc' parameter from the query string
+    transaction = request.args.get('Transaction')  # Get the 'sfc' parameter from the query string
+    if 'MultipleStartSFCAPI' in transaction:
+        inputJson = request.args.get('InputJSON')
+        operation = request.args.get('Operation')
+        username = request.args.get('Username')
+        outputParameter = request.args.get('Output')
+        contentType = request.args.get('content-type')
+    elif 'assembleComponentTrx' in transaction:
+        sfc = request.args.get('SFC')
+        component = request.args.get('Component')
+        assyDataType = request.args.get('Assy_DataType')
     if not sfcNo:  # Handle missing 'sfc' parameter
         return Response("<response><status>Failure</status><message>'sfc' parameter is missing.</message></response>", mimetype='application/xml')
     
